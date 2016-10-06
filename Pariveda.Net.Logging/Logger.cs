f6e697a409b0bf35bcac6ce4ffdf84ae7ce0ada9
Stephens, Nick <NStephens8191@gmail.com>
2016-10-06 12:36:45 -0500

    Initial commit

---
+++ Pariveda.Net.Logging/Logger.cs
@@ -0,0 +1,34 @@
+﻿using NLog;
+
+namespace Pariveda.Net.Logging
+{
+    public class Logger
+    {
+        private static readonly NLog.Logger logger = LogManager.GetCurrentClassLogger();
+
+        public static void Error()
+        {
+            logger.Error("Error");
+        }
+
+        public static void Warning()
+        {
+            logger.Warn("Warning");
+        }
+
+        public static void Info()
+        {
+            logger.Info("Info");
+        }
+
+        public static void Debug()
+        {
+            logger.Debug("Debug");
+        }
+
+        private static string FormatMessage(string message)
+        {
+            return message;
+        }
+    }
+}
